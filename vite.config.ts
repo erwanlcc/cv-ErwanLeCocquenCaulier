f0c1affad316f6d447ebe3da68302e644a28265f
erwanlcc <erwan.lecocquen-caulier@isen-ouest.yncrea.fr>
2025-12-18 08:26:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/cv-ErwanLeCocquenCaulier/", // 🔴 nom EXACT de ton repo GitHub
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
